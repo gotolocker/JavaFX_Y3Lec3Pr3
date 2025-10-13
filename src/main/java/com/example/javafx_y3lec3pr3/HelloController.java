117362553fdb8dec78ee780dbb7ea2e265d23ebc
Locker <kit.zkh@gmail.com>
2025-10-13 16:17:35 +0300

    ui ready

--- src/main/java/com/example/javafx_y3lec3pr3/HelloController.java
+++ src/main/java/com/example/javafx_y3lec3pr3/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.javafx_y3lec3pr3;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -8,7 +9,10 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void btnShow_Click() {
+        Alert info = new Alert(Alert.AlertType.INFORMATION);
+        info.setTitle("Bestie");
+        info.setHeaderText("A?");
+        info.setContentText("sth inside");
     }
 }
